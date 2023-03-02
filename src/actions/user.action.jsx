86effb9a50b4081325e1bcd96d894e7c1baeb67d
unknown <e.cronenberger@gmail.com>
2023-03-03 00:53:32 +0100

    upload et update profil ok

--- src/actions/user.action.jsx
+++ src/actions/user.action.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const GET_USER = "GET_USER";
+export const UPDATE_USER = "UPDATE_USER";
+export const UPLOAD_PICTURE = "UPLOAD_PICTURE ";
+
 
 export const getUser = (uid) => {
   return (dispatch) => {
@@ -12,3 +15,31 @@ export const getUser = (uid) => {
       .catch ((err) => console.log(err));
   };
 };
+
+export const updateUser = (userData) => {
+  return (dispatch, getState) => {
+    const { userReducer: { _id } } = getState(); // récupère l'ID de l'utilisateur actuel depuis le state
+    return axios
+      .put(`${process.env.REACT_APP_API_URL}api/user/${_id}`, userData) // envoie une requête PUT avec les nouvelles données de l'utilisateur
+      .then((res) => {
+        dispatch({ type: UPDATE_USER, payload: userData }); // met à jour le state Redux avec les nouvelles données de l'utilisateur
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const uploadPictures= (data, id) => {
+  return (dispatch) => {
+    return axios
+      .post(`${process.env.REACT_APP_API_URL}api/user/upload`, data)
+      .then((res) => {
+        return axios 
+        .get(`${process.env.REACT_APP_API_URL}api/user/${id}`)
+        .then((res) => {
+          dispatch({ type: UPLOAD_PICTURE, payload: res.data.picture });
+        })
+      })
+     .catch ((err) => console.log(err));
+  };
+};
+
--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -1,39 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Avatar, Button, TextField } from '@mui/material';
-import { updateUser } from '../actions/updateUser.action';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Avatar, Button, TextField } from "@mui/material";
+import { Grid, Box, Typography } from "@mui/material";
+import { updateUser, uploadPictures } from "../actions/user.action";
 
 function Profil() {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.userReducer);
+  const userData = useSelector((state) => state.userReducer);
 
-  const [bio, setBio] = useState(localStorage.getItem('bio') || user.bio);
+  const [bio, setBio] = useState(localStorage.getItem("bio") || userData.bio);
+  const [image, setImage] = useState(null);
+  const [avatarKey, setAvatarKey] = useState(Date.now());
 
   const handleBioChange = (event) => {
     setBio(event.target.value);
+  };
+
+  const handlePictureChange = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    setImage(file);
+  
+    const formData = new FormData();
+    formData.append("name", userData.pseudo);
+    formData.append("userId", userData._id);
+    formData.append("file", file);
+  
+    dispatch(uploadPictures(formData, userData._id))
+      .then(() => {
+        // Ajouter une clé unique pour actualiser la source de l'image
+        const newKey = Date.now();
+        setAvatarKey(newKey);
+      })
+      .catch((error) => console.log(error));
   };
 
   const handleUpdateUser = () => {
     const userData = { bio };
     dispatch(updateUser(userData));
   };
-
+ 
   useEffect(() => {
-    localStorage.setItem('bio', bio);
+    localStorage.setItem("bio", bio);
   }, [bio]);
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Avatar alt={user.pseudo} src={user.picture} style={{ width: '12.5rem', height: '12.5rem', margin: '2rem' }} />
-      <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>{user.pseudo}</div>
-      <div>Membre depuis {new Date(user.createdAt).getFullYear()}</div>
-      <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%', marginTop: '1rem' }}>
-        <div style={{ fontWeight: 'bold' }}>Amis: {user.following ? user.following.length : 0}</div>
-        <div style={{ fontWeight: 'bold' }}>Abonnés: {user.followers ? user.followers.length : 0}</div>
-      </div>
+    <Grid
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+<Avatar
+  src={userData.picture ? `${process.env.REACT_APP_API_URL}${userData.picture}?key=${avatarKey}` : null}
+  style={{ width: "12.5rem", height: "12.5rem", margin: "2rem" }}
+/>
 
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '70%', marginTop: '1rem' }}>
-        <div style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>Bio</div>
+      <input
+        accept=".jpg, .jpeg, .png"
+        style={{ display: "none" }}
+        id="avatar-input"
+        type="file"
+        name="file"
+        onChange={handlePictureChange}
+      />
+      <label htmlFor="avatar-input">
+        <Button
+          variant="contained"
+          component="span"
+          style={{ marginBottom: "1rem" }}
+        >
+          Changer d'image
+        </Button>
+      </label>
+      <Box style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+        {userData.pseudo}
+      </Box>
+      <Box>Membre depuis {new Date(userData.createdAt).getFullYear()}</Box>
+      <Grid
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          width: "100%",
+          marginTop: "1rem",
+        }}
+      >
+        <Box style={{ fontWeight: "bold" }}>
+          Amis: {userData.following ? userData.following.length : 0}
+        </Box>
+        <Box style={{ fontWeight: "bold" }}>
+          Abonnés: {userData.followers ? userData.followers.length : 0}
+        </Box>
+      </Grid>
+
+      <Grid
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "70%",
+          marginTop: "1rem",
+        }}
+      >
+        <Box style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Bio</Box>
         <TextField
           variant="outlined"
           fullWidth
@@ -41,11 +106,13 @@ function Profil() {
           onChange={handleBioChange}
           multiline
           rows={4}
-          style={{ marginBottom: '1rem' }}
+          style={{ marginBottom: "1rem" }}
         />
-        <Button variant="contained" color="primary" onClick={handleUpdateUser}>Enregistrer</Button>
-      </div>
-    </div>
+        <Button variant="contained" color="primary" onClick={handleUpdateUser}>
+          Enregistrer
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
