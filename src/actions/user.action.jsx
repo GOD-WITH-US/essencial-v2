bd7417da024d5e4cc5c6285d6695a118de36da37
unknown <e.cronenberger@gmail.com>
2023-03-14 22:23:49 +0100

    maj profil + add comments

--- src/actions/user.action.jsx
+++ src/actions/user.action.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const GET_USER = "GET_USER";
-export const UPDATE_USER = "UPDATE_USER";
 export const UPLOAD_PICTURE = "UPLOAD_PICTURE ";
-
+export const UPDATE_BIO = "UPDATE_BIO";
+export const FOLLOW_USER = "FOLLOW_USER";
+export const UNFOLLOW_USER = "UNFOLLOW_USER";
 
 export const getUser = (uid) => {
   return (dispatch) => {
@@ -16,13 +17,13 @@ export const getUser = (uid) => {
   };
 };
 
-export const updateUser = (userData) => {
+export const updateBio = (data) => {
   return (dispatch, getState) => {
     const { userReducer: { _id } } = getState(); // récupère l'ID de l'utilisateur actuel depuis le state
     return axios
-      .put(`${process.env.REACT_APP_API_URL}api/user/${_id}`, userData) // envoie une requête PUT avec les nouvelles données de l'utilisateur
+      .put(`${process.env.REACT_APP_API_URL}api/user/${_id}`, data) // envoie une requête PUT avec les nouvelles données de l'utilisateur
       .then((res) => {
-        dispatch({ type: UPDATE_USER, payload: userData }); // met à jour le state Redux avec les nouvelles données de l'utilisateur
+        dispatch({ type: UPDATE_BIO, payload: res.data.bio}); // met à jour le state Redux avec les nouvelles données de l'utilisateur
       })
       .catch((err) => console.log(err));
   };
@@ -43,3 +44,30 @@ export const uploadPictures= (data, id) => {
   };
 };
 
+export const followUser = (followerId, idToFollow) => {
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/user/follow/` + followerId,
+      data: { idToFollow },
+    })
+      .then((res) => {
+        dispatch({ type: FOLLOW_USER, payload: { idToFollow } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const unfollowUser = (followerId, idToUnfollow) => {
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/user/unfollow/` + followerId,
+      data: { idToUnfollow },
+    })
+      .then((res) => {
+        dispatch({ type: UNFOLLOW_USER, payload: { idToUnfollow } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -2,56 +2,71 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Avatar, Button, TextField } from "@mui/material";
 import { Grid, Box, Typography } from "@mui/material";
-import { updateUser, uploadPictures } from "../actions/user.action";
+import { uploadPictures, updateBio } from "../actions/user.action";
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 
 function Profil() {
   const dispatch = useDispatch();
   const userData = useSelector((state) => state.userReducer);
 
+  // Etat local pour stocker la biographie de l'utilisateur
   const [bio, setBio] = useState(localStorage.getItem("bio") || userData.bio);
+
+  // Etat local pour stocker l'image sélectionnée par l'utilisateur
   const [image, setImage] = useState(null);
+
+  // Etat local pour stocker la clé unique qui actualise l'image de l'avatar de l'utilisateur
   const [avatarKey, setAvatarKey] = useState(Date.now());
 
+  // Gérer le changement de la biographie de l'utilisateur
   const handleBioChange = (event) => {
     setBio(event.target.value);
   };
 
+  // Gérer le changement de l'image de l'utilisateur
   const handlePictureChange = (event) => {
     event.preventDefault();
     const file = event.target.files[0];
     setImage(file);
   
+    // Créer un objet FormData pour envoyer les informations de l'image sélectionnée au serveur
     const formData = new FormData();
     formData.append("name", userData.pseudo);
     formData.append("userId", userData._id);
     formData.append("file", file);
   
+    // Appeler l'action "uploadPictures" qui envoie l'image au serveur pour la stocker
     dispatch(uploadPictures(formData, userData._id))
       .then(() => {
-        // Ajouter une clé unique pour actualiser la source de l'image
+        // Ajouter une clé unique pour actualiser la source de l'image de l'avatar
         const newKey = Date.now();
         setAvatarKey(newKey);
       })
       .catch((error) => console.log(error));
   };
 
-  const handleUpdateUser = () => {
+  // Gérer la mise à jour des informations de l'utilisateur
+  const handleBioUpdate = () => {
     const userData = { bio };
-    dispatch(updateUser(userData));
+    dispatch(updateBio(userData));
   };
  
+  // Sauvegarder la biographie de l'utilisateur dans le localStorage
   useEffect(() => {
     localStorage.setItem("bio", bio);
   }, [bio]);
+
   return (
     <Grid
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-<Avatar
-  src={userData.picture ? `${process.env.REACT_APP_API_URL}${userData.picture}?key=${avatarKey}` : null}
-  style={{ width: "12.5rem", height: "12.5rem", margin: "2rem" }}
-/>
-
+      {/* Afficher l'avatar de l'utilisateur */}
+      <Avatar
+        src={userData.picture ? `${process.env.REACT_APP_API_URL}${userData.picture}?key=${avatarKey}` : null}
+        style={{ width: "12.5rem", height: "12.5rem", margin: "2rem" }}
+      />
+  
+      {/* Créer un champ pour sélectionner une nouvelle image */}
       <input
         accept=".jpg, .jpeg, .png"
         style={{ display: "none" }}
@@ -60,19 +75,22 @@ function Profil() {
         name="file"
         onChange={handlePictureChange}
       />
+      {/* Associer la sélection de fichier à un bouton */}
       <label htmlFor="avatar-input">
-        <Button
-          variant="contained"
-          component="span"
-          style={{ marginBottom: "1rem" }}
-        >
-          Changer d'image
-        </Button>
+      <Button variant="contained" component="span" startIcon={<AddAPhotoIcon />} sx={{ mb: 1 }}>
+             Ajouter une photo
+          </Button>
       </label>
+      
+      {/* Afficher le pseudo de l'utilisateur */}
       <Box style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
         {userData.pseudo}
       </Box>
+      
+      {/* Afficher la date de création du compte de l'utilisateur */}
       <Box>Membre depuis {new Date(userData.createdAt).getFullYear()}</Box>
+      
+      {/* Afficher le nombre de personnes suivies et d'abonnés */}
       <Grid
         style={{
           display: "flex",
@@ -82,13 +100,14 @@ function Profil() {
         }}
       >
         <Box style={{ fontWeight: "bold" }}>
-          Amis: {userData.following ? userData.following.length : 0}
+          Suivi(e): {userData.following ? userData.following.length : 0}
         </Box>
         <Box style={{ fontWeight: "bold" }}>
           Abonnés: {userData.followers ? userData.followers.length : 0}
         </Box>
       </Grid>
-
+  
+      {/* Afficher le champ de bio de l'utilisateur */}
       <Grid
         style={{
           display: "flex",
@@ -99,6 +118,7 @@ function Profil() {
         }}
       >
         <Box style={{ fontSize: "1.5rem", fontWeight: "bold" }}>Bio</Box>
+        {/* Créer un champ pour modifier la bio de l'utilisateur */}
         <TextField
           variant="outlined"
           fullWidth
@@ -108,12 +128,14 @@ function Profil() {
           rows={4}
           style={{ marginBottom: "1rem" }}
         />
-        <Button variant="contained" color="primary" onClick={handleUpdateUser}>
+        {/* Ajouter un bouton pour enregistrer les modifications de la bio */}
+        <Button variant="contained" color="primary" onClick={handleBioUpdate}>
           Enregistrer
         </Button>
       </Grid>
     </Grid>
   );
+  
 }
 
 export default Profil;
